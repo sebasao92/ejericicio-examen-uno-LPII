db43946e3b279f12cbfe17d847802dbd69a3efaa
sebasao92 <39807445+sebasao92@users.noreply.github.com>
2021-03-10 19:53:10 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hice un cambio");
         new RecibidorDeInformacion().recibirInformacion();
     }
 }
