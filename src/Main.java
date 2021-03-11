d22b77ee612960014e6d8e7e6025c82618022247
sebasao92 <sealor92@gmail.com>
2021-03-10 19:50:42 -0500

    modificación del main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Hice un cambio");
         new RecibidorDeInformacion().recibirInformacion();
     }
 }
